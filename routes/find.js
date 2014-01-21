6316e246c5b25441aac403e73a49f6600e180f1c
Harpagon <harpagonzp@gmail.com>
2014-01-21 15:54:47 +0200

    Проба рендеринга страницы

--- routes/find.js
+++ routes/find.js
@@ -1,8 +1,8 @@
 
 /*
- * GET users listing.
+ * GET home page.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+exports.find = function(req, res){
+  res.render('find', { title: 'Что ищем?', vars: [{id:'id1',name:'name1'},{id:'id2',name:'name2'}] });
 };
